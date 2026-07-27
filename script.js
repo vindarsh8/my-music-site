a8adb77f8bca46908fe4cb580c4a34ec0cd5935b
vindarsh8 <vindarsh8@gmail.com>
2026-07-27 21:59:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 ================================ */
 
 // Change this to whatever 4-digit code you want
-const PASSCODE = "1234";
+const PASSCODE = "0000";
 
 /* ===============================
    ELEMENT REFERENCES
